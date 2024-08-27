620188cf7620f00a64fa2304007ef43dcef3681a
Ernest Guevarra <ernest@guevarra.io>
2024-08-27 03:21:34 +0100

    add tests for pact_download

---
+++ tests/testthat/test-pact_download.R
@@ -0,0 +1,20 @@
+# Tests for pact_download functdion --------------------------------------------
+
+## Set client ----
+pact_client <- pact_client_set()
+
+download_file <- pact_download(pact_client, id = 24763548, path = tempdir())
+
+## Tests ----
+test_that("pact_download outputs as expected", {
+  expect_equal(
+    download_file,
+    file.path(
+      tempdir(), "PandemicPACT-GloPIDRAndUKC_DemoData-Label_21-11-23.csv"
+    )
+  )
+  expect_type(download_file, "character")
+  expect_true(
+    "PandemicPACT-GloPIDRAndUKC_DemoData-Label_21-11-23.csv" %in% list.files(tempdir())
+  )
+})
